886c874501c4d09c2ef7f8987322b36cfd4ee011
kristapants <peachykeen@gmail.com>
2018-12-08 15:58:39 -0600

    Banner image, slides and conditional formatting of landing page link

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,6 +9,9 @@
   <meta name="description" content="<?= $site->description()->html() ?>">
   <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet" />
+  <?php echo css('assets/revolution/css/settings.css')?>
+  <?php echo css('assets/revolution/css/layers.css')?>
+  <?php echo css('assets/revolution/css/navigation.css')?>
   <?php echo css('assets/css/main.css')?>
 
 </head>
